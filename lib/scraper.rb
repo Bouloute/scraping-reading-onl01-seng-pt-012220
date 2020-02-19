a613c1a24333b30b18814434f3f9805f20271f5c
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-19 22:33:27 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,13 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+puts "name"
 p courses[0].name
+
+puts "attributes"
 p courses[0].attributes
+
+puts "stripped text"
 courses.each do |course|
    puts course.text.strip
 end
